ca7934b8b2e4124019bc0cd085ea736686282d90
ethanblauw21 <134352740+ethanblauw21@users.noreply.github.com>
2025-03-20 15:28:43 -0500

    first commit

---
+++ src/main/java/com/tryingthisout/App.java
@@ -0,0 +1,16 @@
+package com.tryingthisout;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        System.out.println(EvenOrOdd.evenOrOdd(sc.nextInt()));
+        sc.close();
+    }
+}
---
+++ src/main/java/com/tryingthisout/EvenOrOdd.java
@@ -0,0 +1,7 @@
+package com.tryingthisout;
+
+public class EvenOrOdd {
+    public static String evenOrOdd(int n){
+        return (n % 2 == 0)? "Number " + n + " is even" : "Number " + n + " is odd";
+    }
+}
