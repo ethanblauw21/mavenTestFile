48085addb37f6070e61a4f3d339229fd5b71e0f7
ethanblauw21 <134352740+ethanblauw21@users.noreply.github.com>
2025-03-21 14:26:35 -0500

    look at me adding tests

--- src/test/java/com/tryingthisout/AppTest.java
+++ src/test/java/com/tryingthisout/AppTest.java
@@ -1,5 +1,6 @@
 package com.tryingthisout;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,6 +13,16 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    @Test
+    public void evenOrOddTestEven(){
+        assertEquals("Number 66 is even", EvenOrOdd.evenOrOdd(66));   
+    }
+
+    @Test
+    public void evenOrOddTestOdd(){
+        assertEquals("Number 65 is odd", EvenOrOdd.evenOrOdd(65));
+    }
+    
     @Test
     public void shouldAnswerWithTrue()
     {
